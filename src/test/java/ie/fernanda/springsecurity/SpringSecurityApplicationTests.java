1e7d069b6d00d869c244d9e272975b1015c12b29
Fernanda Rocha <fernandaporocha@gmail.com>
2021-02-03 16:07:05 +0000

    Starting project

---
+++ src/test/java/ie/fernanda/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.fernanda.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
